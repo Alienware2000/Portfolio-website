131e3d33ffcfa078bddb60c8ea7604859ceb913f
David Antwi <antwidavid389@gmail.com>
2025-11-09 03:51:11 -0500

    fixed navbar issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import ProjectDetail from "./components/ProjectDetail.jsx";
 import Atmosphere from "./components/Atmosphere.jsx";
 import Navbar from "./components/Navbar.jsx";
 
-export default function App() {
+function Shell() {
+  const location = useLocation();
+  const showNavbar = location.pathname === "/";
   return (
-    <BrowserRouter>
-      {/* Persist background and navbar across routes to avoid remount cost */}
+    <>
       <Atmosphere />
-      <Navbar />
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:slug" element={<ProjectDetail />} />
         <Route path="*" element={<Home />} />
       </Routes>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Shell />
     </BrowserRouter>
   );
 }
